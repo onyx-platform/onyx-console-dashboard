8497c4a3715d0b414c6137c7e54330e8271c1cbf
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-28 23:20:45 +0800

    Allow edn reading as well as ZK pull functionality

--- src/onyx_console_dashboard/core.clj
+++ src/onyx_console_dashboard/core.clj
@@ -2,14 +2,15 @@
   (:require [lanterna.screen :as s]
             [onyx.system]
             [onyx.extensions :as extensions]
+            [onyx.api]
             [clojure.data :refer [diff]]
+            [clojure.core.async :refer [chan >!! <!! close! alts!! timeout go]]
             [fipp.clojure :as fipp]
-            [onyx.log.replica :refer  [base-replica]])
+            [onyx.log.replica :refer [base-replica]])
   (:gen-class))
 
 (def scr (s/get-screen))
 
-;; Put into lib-onyx
 (defn replicas
   [job-scheduler messenger peer-log]
   (vec 
@@ -26,8 +27,6 @@
                                 :else x)) 
                         peer-log))
 
-
-
 (defn render-lines [scr y-start lines color]
   (run! (fn [[y line-str]]
           (s/put-string scr 0 y line-str {:fg color})) 
@@ -35,12 +34,14 @@
              (range y-start 100000)
              lines)))
 
-(defn render-loop! [scr peer-log replicas]
+(def state (atom nil))
+
+(defn render-loop! [scr]
   (loop [[cols rows] (s/get-size scr)
          key-val :unhandled 
          curr-entry 0]
 
-    (when (= key-val :escape)
+    (when (#{:escape \q} key-val)
       (System/exit 0)
       (s/stop scr))
 
@@ -49,7 +50,7 @@
     (let [new-curr-entry (cond 
                            (and (= key-val :down)
                                 (< curr-entry 
-                                   (dec (count peer-log))))
+                                   (dec (count (:log @state)))))
                            (inc curr-entry)
                            (and (= key-val :up)
                                 (pos? curr-entry))
@@ -57,11 +58,11 @@
                            :else curr-entry)
           entry-lines (clojure.string/split 
                         (with-out-str (fipp/pprint 
-                                        (nth peer-log new-curr-entry)
+                                        ((:log @state) new-curr-entry)
                                         {:width cols})) #"\n")
           replica-lines (clojure.string/split 
                           (with-out-str (fipp/pprint 
-                                          (replicas new-curr-entry)                                     
+                                          ((:replicas @state) new-curr-entry)                                     
                                           {:width cols})) #"\n")]
 
       (render-lines scr 0 [(str new-curr-entry)] :red)
@@ -74,15 +75,37 @@
              (s/get-key-blocking scr) 
              new-curr-entry))))
 
+(defn import-zookeeper! [peer-config onyx-id]
+  (future 
+    (let [ch (chan)
+          started-sub (-> peer-config
+                          (assoc :onyx/id onyx-id)
+                          (onyx.api/subscribe-to-log ch))] 
+      (loop [entry (<!! ch)]
+        (swap! state update :log conj entry)
+        (swap! state update :replicas (fn [replicas] (conj replicas (extensions/apply-log-entry entry (last replicas)))))
+        (recur (<!! ch))))))
+
+(defn import-edn-log! [filename]
+  (let [peer-log (->> (slurp filename)
+                      read-string
+                      sanitize-peer-log)
+        replicas (replicas :onyx.job-scheduler/greedy :aeron peer-log)]
+    (reset! state {:replicas replicas :log peer-log})))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  [& [type src]]
 
-  (s/start scr)
-  (let [peer-log (->> (slurp "/Users/lucas/onyx/onyx/peer.edn")
-                      read-string
-                      sanitize-peer-log)
-        
-        replicas (replicas :onyx.job-scheduler/greedy :aeron peer-log)] 
-    (render-loop! scr peer-log replicas)))
+  (let [peer-config (read-string (slurp "peer-config.edn"))]
+    (reset! state {:replicas [(assoc base-replica
+                                     :job-scheduler (:onyx.peer/job-scheduler peer-config)
+                                     :messaging {:onyx.messaging/impl (:onyx.messaging/impl peer-config)})]
+
+                   :log [nil]})
+    (case type
+      "edn" (import-edn-log! src)
+      "zookeeper" (import-zookeeper! peer-config src))
+
+    (s/start scr)
+    (render-loop! scr)))
