496d3b37e418cc693e5d27314dd9a82f7884ceb2
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-02 14:48:52 +0800

    Add support for diff viewing

--- src/onyx_console_dashboard/core.clj
+++ src/onyx_console_dashboard/core.clj
@@ -3,7 +3,7 @@
             [onyx.system]
             [onyx.extensions :as extensions]
             [onyx.api]
-            [clojure.data :refer [diff]]
+            [clojure.data]
             [clojure.core.async :refer [chan >!! <!! close! alts!! timeout go]]
             [fipp.clojure :as fipp]
             [onyx.log.replica :refer [base-replica]])
@@ -14,11 +14,12 @@
 (defn replicas
   [job-scheduler messenger peer-log]
   (vec 
-    (reductions #(extensions/apply-log-entry %2 %1)
-                (assoc base-replica
-                       :job-scheduler job-scheduler
-                       :messaging {:onyx.messaging/impl messenger})
-                peer-log)))
+    (rest 
+      (reductions #(extensions/apply-log-entry %2 %1)
+                  (assoc base-replica
+                         :job-scheduler job-scheduler
+                         :messaging {:onyx.messaging/impl messenger})
+                  peer-log))))
 
 (defn sanitize-peer-log [peer-log]
   (clojure.walk/prewalk (fn [x] 
@@ -39,35 +40,58 @@
 (defn render-loop! [scr]
   (loop [[cols rows] (s/get-size scr)
          key-val :unhandled 
-         curr-entry 0]
+         curr-entry 1]
 
     (when (#{:escape \q} key-val)
       (System/exit 0)
       (s/stop scr))
 
+
+    (cond (= key-val \d)
+          (swap! state assoc :mode :diff)
+
+          (= key-val \r)
+          (swap! state assoc :mode :replica))
+
     (s/clear scr)
 
     (let [new-curr-entry (cond 
-                           (and (= key-val :down)
-                                (< curr-entry 
-                                   (dec (count (:log @state)))))
-                           (inc curr-entry)
-                           (and (= key-val :up)
-                                (pos? curr-entry))
-                           (dec curr-entry)
+                           (= key-val :page-down)
+                           (min (dec (count (:log @state)))
+                                (+ curr-entry 10))
+
+                           (and (= key-val :down))
+                           (min (dec (count (:log @state)))
+                                (inc curr-entry))
+
+                           (= key-val :page-up)
+                           (max 0 (- curr-entry 10))
+                           
+                           (= key-val :up)
+                           (max 0 (dec curr-entry))
+
                            :else curr-entry)
           log-entry ((:log @state) new-curr-entry)
           replica ((:replicas @state) new-curr-entry)                                     
+          ;;; Add some children functonality
+          prev-replica (get (:replicas @state) (dec new-curr-entry) base-replica)
+          ;; TODO, show both
+          diff (clojure.data/diff prev-replica replica)
           entry-lines (clojure.string/split 
                         (with-out-str (fipp/pprint log-entry
                                                    {:width cols})) #"\n")
           replica-lines (clojure.string/split 
                           (with-out-str (fipp/pprint replica
-                                                     {:width cols})) #"\n")]
+                                                     {:width cols})) #"\n")
+          diff-lines (clojure.string/split 
+                       (with-out-str (fipp/pprint diff
+                                                  {:width cols})) #"\n")]
 
       (render-lines scr 0 [(str new-curr-entry " - " (java.util.Date. (:created-at log-entry)))] :red)
       (render-lines scr 1 entry-lines :green)
-      (render-lines scr (+ 2 (count entry-lines)) replica-lines :white)
+      (case (:mode @state)
+        :diff (render-lines scr (+ 2 (count entry-lines)) diff-lines :white)
+        :replica (render-lines scr (+ 2 (count entry-lines)) replica-lines :white))
 
       (s/redraw scr)
 
@@ -91,17 +115,18 @@
 
 (defn import-peer-log! [peer-log]
   (let [replicas (replicas :onyx.job-scheduler/greedy :aeron (sanitize-peer-log peer-log))]
-    (reset! state {:replicas replicas :log peer-log})))
+    (reset! state {:replicas replicas 
+                   :mode :replica
+                   :log peer-log})))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& [type src]]
 
   (let [peer-config (read-string (slurp "peer-config.edn"))]
     (reset! state {:replicas [(assoc base-replica
                                      :job-scheduler (:onyx.peer/job-scheduler peer-config)
                                      :messaging {:onyx.messaging/impl (:onyx.messaging/impl peer-config)})]
-
+                   :mode :replica
                    :log [nil]})
     (case type
       "edn" (import-peer-log! (slurp-edn src))
